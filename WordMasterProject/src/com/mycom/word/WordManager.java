8fee018eae1f9e54a26004983daef999ee6457e1
Cocomong98 <90203932+Cocomong98@users.noreply.github.com>
2022-09-17 23:46:10 +0900

    WordManager - 최종수정

--- WordMasterProject/src/com/mycom/word/WordManager.java
+++ WordMasterProject/src/com/mycom/word/WordManager.java
@@ -1,195 +1,61 @@
 package com.mycom.word;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class WordCRUD implements ICRUD{
+public class WordManager {
+	Scanner s = new Scanner(System.in);
+	WordCRUD wordCRUD;
 	
-	ArrayList<Word> list;
-	Scanner s;
-	final String fname = "Dictionary.txt";
-	
-	WordCRUD(Scanner s) { 
-		list = new ArrayList<>();
-		this.s = s;
+	WordManager() {
+		wordCRUD = new WordCRUD(s);
 	}
-
-	@Override
-	public Object add() {
-		System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
-		int level = s.nextInt();
-		String word = s.nextLine();
+	
+	public int selectMenu() {
+		System.out.print("*** 영단어 마스터 ***\n"
+				+ "********************\n"
+				+ "*** 1. 모든 단어 보기 ***\n"
+				+ "*** 2. 수준별 단어 보기 ***\n"
+				+ "*** 3. 단어 검색 ***\n"
+				+ "*** 4. 단어 추가 ***\n"
+				+ "*** 5. 단어 수정 ***\n"
+				+ "*** 6. 단어 삭제 ***\n"
+				+ "*** 7. 파일 저장 ***\n"
+				+ "*** 0. 나가기 ***\n"
+				+ "********************\n"
+				+ "=> 원하는 메뉴는? ");
 		
-		System.out.print("뜻 입력 : ");
-		String meaning = s.nextLine();
+		return s.nextInt();
+	}
+	
+	public void start() {
 		
-		
-		return new Word(0, level, word, meaning);
-	}
-	
-	public void addItem() {
-		Word one = (Word)add();
-		list.add(one);
-		System.out.println("새 단어가 단어장에 추가되었습니다. ");
-	}
-
-	@Override
-	public int update(Object obj) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int delete(Object obj) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void selectOne(int id) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void listAll() {
-		System.out.println("--------------------------------");
-		for (int i=0; i<list.size(); i++) {
-			System.out.print((i+1) + " ");
-			System.out.println(list.get(i).toString());
-		}
-		System.out.println("--------------------------------");
-	}
-	
-	
-	public ArrayList<Integer> listAll (String keyword) {
-		ArrayList<Integer> idlist = new ArrayList();
-		int j=0;
-		
-		System.out.println("--------------------------------");
-		for (int i=0; i<list.size(); i++) {
-			String word = list.get(i).getWord();
-			if(!word.contains(keyword)) continue;
-			System.out.print((i+1) + " ");
-			System.out.println(list.get(i).toString());
-			idlist.add(i);
-			j++;
-		}
-		System.out.println("--------------------------------");
-		return idlist;
-	}
-	
-public void listAll(int level) {
-		int j=0;
-	
-		System.out.println("--------------------------------");
-		for (int i=0; i<list.size(); i++) {
-			int ilevel = list.get(i).getLevel();
-			if(ilevel != level) continue;
-			System.out.print((i+1) + " ");
-			System.out.println(list.get(i).toString());
-			j++;
-		}
-		System.out.println("--------------------------------");
-	}
-
-	public void updateItem() {
-		// TODO Auto-generated method stub
-		System.out.print("=> 수정할 단어 검색 : ");
-		String keyword = s.next();
-		ArrayList<Integer> idlist = this.listAll(keyword);
-		System.out.print("=> 수정할 번호 선택 : ");
-		int id = s.nextInt();
-		s.nextLine();
-		
-		System.out.print("=> 뜻 입력 : ");
-		String meaning = s.nextLine();
-		Word word = list.get(idlist.get(id-1));
-		word.setMeaning(meaning);
-		System.out.print("=> 단어가 수정되었습니다. ");
-	}
-
-	public void deleteItem() {
-		// TODO Auto-generated method stub
-		System.out.print("=> 삭제할 단어 검색 : ");
-		String keyword = s.next();
-		ArrayList<Integer> idlist = this.listAll(keyword);
-		System.out.print("=> 삭제할 번호 선택 : ");
-		int id = s.nextInt();
-		s.nextLine();
-		
-		System.out.print("=> 정말로 삭제하시겠습니까? (Y/n) ");
-		String ans = s.nextLine();
-		if(ans.equalsIgnoreCase("y")) {
-			list.remove((int)idlist.get(id-1));
-			System.out.print(" 단어가 삭제되었습니다. ");
-		}
-		else {
-			System.out.print(" 단어 삭제가 취소되었습니다. ");
-		}
-	}
-	
-	public void loadFile() {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(fname));
-			String line;
-			int count = 0;
-			
-			while(true) {
-				line = br.readLine();
-				if (line == null) break;
-				
-				String data[] = line.split("\\|");
-				int level = Integer.parseInt(data[0]);
-				String word = data[1];
-				String meaning = data[2];
-				list.add(new Word(0, level, word, meaning));
-				count++;
+		wordCRUD.loadFile();
+		while (true) {
+			int menu = selectMenu();
+			if(menu ==0) {System.out.print("프로그램 종료! 다음에 만나요~"); break;}
+			if(menu ==4) {
+				wordCRUD.addItem();
 			}
-			br.close();
-			System.out.println("==> " + count + "개 로딩 완료!!!");
-			
-		}	catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void saveFile() {
-		// TODO Auto-generated method stub
-		try {
-			PrintWriter pr = new PrintWriter(new FileWriter ("fname"));
-			for(Word one : list) {
-				pr.write(one.toFileString()+"\n");
+			else if	(menu ==1) {
+				wordCRUD.listAll();
 			}
-			pr.close();
-			System.out.println("==> 데이터 저장 완료!!!");
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			else if	(menu ==2) {
+				wordCRUD.searchLevel();
+			}
+			else if	(menu ==3) {
+				wordCRUD.searchWord();
+			}
+			else if (menu ==5) { // 추가
+				wordCRUD.updateItem();
+			}
+			else if (menu ==6) { // 삭제
+				wordCRUD.deleteItem();
+			}
+			else if (menu == 7) {
+				wordCRUD.saveFile();
+			}
 		}
 		
 	}
-
-	public void searchLevel() {
-		// TODO Auto-generated method stub
-		System.out.print("=> 원하는 레벨은? (1~3) ");
-		int level = s.nextInt();
-		listAll(level);
-	}
-
-	public void searchWord() {
-		// TODO Auto-generated method stub
-		System.out.print("=> 원하는 단어는?");
-		String keyword = s.next();
-		listAll(keyword);
-	}
-
 	
-	
-
 }
